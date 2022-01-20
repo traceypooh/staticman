cda323b71361d46e2ebe68e26a34c7a8c54d0e19
Tracey Jaquith <tracey@archive.org>
2022-01-19 17:15:45 -0800

    switch healthcheck from HTTP to TCP.  prestart setup should be pretty close to working now

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,10 @@
 #!/bin/bash -e
 
-pwd
-env; # xxx
-
 # create our config on-demand from our runtime-only environment variables passed in to us
-cat > config.production.json <<EOF
+cat >| config.production.json <<EOF
 {
-  "gitlabToken": "$SEKRIT",
-  "githubToken": "$SEKRIT"
-  "rsaPrivateKey": "-----BEGIN RSA PRIVATE KEY-----$SEKRIT-----END RSA PRIVATE KEY-----",
+  "gitlabToken": "$GLTOK",
+  "rsaPrivateKey": "$RSA",
   "port": 80
 }
 EOF
