78db68ed1555c5f72d66e1fdce9c277958963c7a
Tracey Jaquith <tracey@archive.org>
2025-01-25 22:13:16 -0800

    comments, re-indents, etc (nothing substantitive)

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,29 +2,30 @@
 
 # create our config on-demand from our runtime-only environment variables passed in to us
 
-# the key env vars need to be base64 encoded for transport, and we'll unpack it to:
-#   "-----BEGIN RSA PRIVATE KEY-----\n[..KEY..]\n-----END RSA PRIVATE KEY-----"
-
 export NODE_ENV=production
-export PORT=80
-export GITHUB_APP_ID=$APP_ID
+
+#export PORT=80
+#export GITHUB_APP_ID=$APP_ID
 # for this first one, eg: https://travistidwell.com/jsencrypt/demo/
-export RSA_PRIVATE_KEY=$(echo "$RSA_PKEY" |sed 's/NEWLINE/\\n/g')
-export GITHUB_PRIVATE_KEY=$(echo "$RSA" |sed 's/NEWLINE/\\n/g')
-# export GITHUB_PRIVATE_KEY=$(echo "$RSA" |base64 -d|sed ':a;N;$!ba;s/\n/\\n/g')
+#export RSA_PRIVATE_KEY=$(echo "$RSA_PKEY" |sed 's/NEWLINE/\\n/g')
+#export GITHUB_PRIVATE_KEY=$(echo "$RSA" |sed 's/NEWLINE/\\n/g')
 
-# cat RSA_PKEY.key |tr '\n' ' ' |perl -pe 's/(KEY-----) /$1NEWLINE/; s/ (-----END)/NEWLINE$1/'
+## the key env vars need to be base64 encoded for transport, and we'll unpack it to:
+##   "-----BEGIN RSA PRIVATE KEY-----\n[..KEY..]\n-----END RSA PRIVATE KEY-----"
+## export GITHUB_PRIVATE_KEY=$(echo "$RSA" |base64 -d|sed ':a;N;$!ba;s/\n/\\n/g')
+## cat RSA_PKEY.key |tr '\n' ' ' |perl -pe 's/(KEY-----) /$1NEWLINE/; s/ (-----END)/NEWLINE$1/'
 
 
-# cat >| config.production.json <<EOF
-# {
-#   "githubToken": "$GHTOK",
-#   "rsaPrivateKey": "$RSA_1LINER",
-#   "port": 80
-# }
-# EOF
+cat >| config.production.json <<EOF
+{
+  "githubAppId": "$APP_ID",
+  "githubPrivateKey": "$RSA",
+  "rsaPrivateKey": "$RSA_PKEY",
+  "port": 80
+}
+EOF
 
-# sed -i 's/NEWLINE/\\n/g' config.production.json
+sed -i 's/NEWLINE/\\n/g' config.production.json
 
 while true; do
   set +e
