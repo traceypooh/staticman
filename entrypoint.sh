577fa05332d466e39fb31b0f71a02f4f8cd48ca9
Tracey Jaquith <tracey@archive.org>
2025-01-25 20:25:11 -0800

    multiline hell

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,16 @@
 #!/bin/zsh -e
 
 # create our config on-demand from our runtime-only environment variables passed in to us
-# eg:
-# rsaPrivateKey: "-----BEGIN RSA PRIVATE KEY-----\nkey\n-----END RSA PRIVATE KEY-----"
+
+# this env var needs to be base64 encoded for transport, and we'll unpack it to:
+# rsaPrivateKey: "-----BEGIN RSA PRIVATE KEY-----\n[..KEY..]\n-----END RSA PRIVATE KEY-----"
+
+RSA_1LINER=$(echo "$RSA" |base64 -d|sed ':a;N;$!ba;s/\n/\\n/g')
 
 cat >| config.production.json <<EOF
 {
   "githubToken": "$GHTOK",
-  "rsaPrivateKey": "$RSA",
+  "rsaPrivateKey": "$RSA_1LINER",
   "port": 80
 }
 EOF
