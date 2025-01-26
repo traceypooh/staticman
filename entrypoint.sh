af726fe72368f8a650563def7f4ff3e9edd61340
Tracey Jaquith <tracey@archive.org>
2025-01-25 20:47:57 -0800

    take 2

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,23 +2,23 @@
 
 # create our config on-demand from our runtime-only environment variables passed in to us
 
-# this env var needs to be base64 encoded for transport, and we'll unpack it to:
-# rsaPrivateKey: "-----BEGIN RSA PRIVATE KEY-----\n[..KEY..]\n-----END RSA PRIVATE KEY-----"
-
-RSA_1LINER=$(echo "$RSA" |base64 -d|sed ':a;N;$!ba;s/\n/\\n/g')
-
-cat >| config.production.json <<EOF
-{
-  "githubToken": "$GHTOK",
-  "rsaPrivateKey": "$RSA_1LINER",
-  "port": 80
-}
-EOF
-
-sed -i 's/NEWLINE/\\n/g' config.production.json
+# the key env var needs to be base64 encoded for transport, and we'll unpack it to:
+#   "-----BEGIN RSA PRIVATE KEY-----\n[..KEY..]\n-----END RSA PRIVATE KEY-----"
 
 export NODE_ENV=production
+export PORT=80
+export GITHUB_PRIVATE_KEY=$(echo "$RSA" |base64 -d|sed ':a;N;$!ba;s/\n/\\n/g')
+export GITHUB_APP_ID=$APP_ID
 
+# cat >| config.production.json <<EOF
+# {
+#   "githubToken": "$GHTOK",
+#   "rsaPrivateKey": "$RSA_1LINER",
+#   "port": 80
+# }
+# EOF
+
+# sed -i 's/NEWLINE/\\n/g' config.production.json
 
 while true; do
   set +e
